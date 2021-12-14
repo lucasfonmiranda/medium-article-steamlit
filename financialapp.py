3db5c07ecbb5b1152584f72926990d4893d73338
Lucas Fonseca <lucasfonsecadatascientist@gmail.com>
2021-12-14 12:03:50 -0300

    feat: article repository

---
+++ financialapp.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Financial App
+
+""")
+
+stock = 'AMZN'
+
+get_stock_data = yf.Ticker(stock)
+
+ticket_df = get_stock_data.history(period='1d', start='2021-1-02', end='2021-12-12')
+
+st.line_chart(ticket_df.Close)
+st.line_chart(ticket_df.Volume)
+
